e2e876b47cef050a75bd4638e149ee414871de1c
KhanTheDev <153256283+KhanTheDev@users.noreply.github.com>
2025-06-01 00:03:27 -0400

    Update index.tsx

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,10 +1,13 @@
-import { Header, ThemedLayoutV2 } from "@refinedev/antd"
+import { Header, ThemedLayoutV2, ThemedTitleV2 } from "@refinedev/antd"
 
-const index = () => { 
+const Layout = ({ children }: React.PropsWithChildren) => { 
     return (
-        <ThemedLayoutV2>
-            Header = {Header}
-            </ThemedLayoutV2>
+        <ThemedLayoutV2
+            Header={Header}
+            Title={(titleProps) => <ThemedTitleV2 {...titleProps} text="Refine" />}
+        >
+            {children}
+        </ThemedLayoutV2>
     )
 }
 
