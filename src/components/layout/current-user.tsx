d86269616788d1569cdea0e9cc2667678d7ceb7f
KhanTheDev <153256283+KhanTheDev@users.noreply.github.com>
2025-06-03 15:13:58 -0400

    added the utilities functions into the src
    contains parameters and getFunctions for the main components of the dashboard

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -5,6 +5,7 @@ import { useGetIdentity } from "@refinedev/core/dist/hooks/auth/useGetIdentity"
 import type {User} from '@graphql/schema.types'
 const CurrentUser = () => { 
     const {data: user} = useGetIdentity<User>()
+
     return (
         <>
 <Popover
@@ -13,7 +14,13 @@ const CurrentUser = () => {
    overlayInnerStyle = {{padding : 0}}
    overlayStyle = {{ zIndex: 999}}
      >
-        <CustomAvatar />
+        <CustomAvatar
+        name = {user?.name}
+        src = {user?.avatarUrl}
+        size = "default"
+        style = {{cursor: 'pointer'}}
+        
+    />
     </Popover>
          </>
     )
