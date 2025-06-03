ca741b385c301f2643f82488e7838ba78f463a4a
KhanTheDev <153256283+KhanTheDev@users.noreply.github.com>
2025-06-02 23:27:57 -0400

    Update custom-avatar.tsx

--- src/components/custom-avatar.tsx
+++ src/components/custom-avatar.tsx
@@ -8,17 +8,18 @@ type Props = AvatarProps & {
 const CustomAvatar = ({name, style, ...rest }: Props) => {
     return (
         <AntdAvatar
-        alt = {'John Doe'}
+        alt = {name}
         size = "small"
         style = {{
         backgroundColor: '#87d068',
         display: 'flex',
         alignItems: 'center',
-        border: 'none'
+        border: 'none',
+        ...style
         }}
         
         >
-            JM
+            {name}
         </AntdAvatar>
     )
 }
