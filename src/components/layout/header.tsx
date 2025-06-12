7a304154a80a7c04de866aa7ee208ae05c38bd04
KhanTheDev <153256283+KhanTheDev@users.noreply.github.com>
2025-06-12 15:22:26 -0400

    user issues

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,13 @@
+import CurrentUser from "./current-user"
+
+const Header = () => { 
+    return (
+        <div>
+            <CurrentUser />
+            
+         </div>
+    )
+    
+}
+
+export default Header
